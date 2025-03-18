4e5c88170673f5dce0f4308a44d9b3cb9c6f4e20
jcl <chaelynjang@lotte.net>
2025-03-18 15:46:53 +0900

    단축 url 조회 api 성능 테스트 - redis를 이용하여 캐시작업, 성능 개선 도모

---
+++ src/main/java/kr/co/shortenurlservice/infrastructure/RedisConfig.java
@@ -0,0 +1,22 @@
+package kr.co.shortenurlservice.infrastructure;
+
+import kr.co.shortenurlservice.domain.ShortenUrl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, ShortenUrl> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, ShortenUrl> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+}
--- src/main/java/kr/co/shortenurlservice/infrastructure/ShortenUrlRepositoryImpl.java
+++ src/main/java/kr/co/shortenurlservice/infrastructure/ShortenUrlRepositoryImpl.java
@@ -3,6 +3,7 @@ package kr.co.shortenurlservice.infrastructure;
 import kr.co.shortenurlservice.domain.ShortenUrl;
 import kr.co.shortenurlservice.domain.ShortenUrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
@@ -13,38 +14,45 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ShortenUrlRepositoryImpl implements ShortenUrlRepository {
 
     private final JpaShortenUrlRepository jpaShortenUrlRepository;
-    private final ConcurrentHashMap<String, ShortenUrl> cache;
+    //private final ConcurrentHashMap<String, ShortenUrl> cache;
+    private final RedisTemplate<String, ShortenUrl> redisTemplate;
+    private static final String CACHE_PREFIX = "shortenUrl::";
 
     @Autowired
-    public ShortenUrlRepositoryImpl(JpaShortenUrlRepository jpaShortenUrlRepository) {
+    public ShortenUrlRepositoryImpl(JpaShortenUrlRepository jpaShortenUrlRepository, RedisTemplate<String, ShortenUrl> redisTemplate) {
         this.jpaShortenUrlRepository = jpaShortenUrlRepository;
-        this.cache = new ConcurrentHashMap<>();
+    //    this.cache = new ConcurrentHashMap<>();
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
     public void saveShortenUrl(ShortenUrl shortenUrl) {
         jpaShortenUrlRepository.save(shortenUrl);
-        cache.put(shortenUrl.getShortenUrlKey(), shortenUrl);
+        //cache.put(shortenUrl.getShortenUrlKey(), shortenUrl);
+        redisTemplate.opsForValue().set(CACHE_PREFIX + shortenUrl.getShortenUrlKey(), shortenUrl);
     }
 
     @Async
     @Override
     public void asyncSaveShortenUrl(ShortenUrl shortenUrl) {
         jpaShortenUrlRepository.save(shortenUrl);
-        cache.put(shortenUrl.getShortenUrlKey(), shortenUrl);
+        //cache.put(shortenUrl.getShortenUrlKey(), shortenUrl);
+        redisTemplate.opsForValue().set(CACHE_PREFIX + shortenUrl.getShortenUrlKey(), shortenUrl);
     }
 
     @Override
     public ShortenUrl findShortenUrlByShortenUrlKey(String shortenUrlKey) {
 //        return jpaShortenUrlRepository.findByShortenUrlKey(shortenUrlKey);
         // 먼저 캐시에서 조회하도록 성능 개선
-        ShortenUrl shortenUrl = cache.get(shortenUrlKey);
+        //ShortenUrl shortenUrl = cache.get(shortenUrlKey);
+        ShortenUrl shortenUrl = redisTemplate.opsForValue().get(CACHE_PREFIX + shortenUrlKey);
         if(shortenUrl == null) {
             // 캐시에 없으면 db에서 조회
             shortenUrl = jpaShortenUrlRepository.findByShortenUrlKey(shortenUrlKey);
             if(shortenUrl != null) {
                 // db에 있으면 캐시에 저장
-                cache.put(shortenUrlKey, shortenUrl);
+                //cache.put(shortenUrlKey, shortenUrl);
+                redisTemplate.opsForValue().set(CACHE_PREFIX + shortenUrl.getShortenUrlKey(), shortenUrl);
             }
         }
         return shortenUrl;
